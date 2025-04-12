5521c9d6d5933d8617ade3150d2bf3349ccf9fc5
anthony <anthony.butcher@samblagroup.com>
2025-04-12 18:56:30 +0200

    Use minimal status line

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,18 +1,21 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "nord",
+  theme = "nord",
   transparency = true,
 
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+}
 
-	hl_override = {
-		Comment = { italic = true },
-		["@comment"] = { italic = true },
-	},
+M.ui = {
+  statusline = { theme = "minimal", separator_style = "round" },
 }
 
 vim.opt.relativenumber = true
