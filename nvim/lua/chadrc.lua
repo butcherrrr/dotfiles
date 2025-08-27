b8980eebdfbaa0fb06ac5181d771bfc07e82b7b3
anthony <anthony.butcher@samblagroup.com>
2025-08-27 19:34:55 +0200

    new daily

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -5,7 +5,7 @@
 local M = {}
 
 M.base46 = {
-  theme = "tokyonight",
+  theme = "nord",
   transparency = true,
 
   hl_override = {
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,13 +5,14 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 20
 config.colors = {
-  -- background = "#1F2531"
+  background = "#2A2A36"
 }
 
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
-config.color_scheme = "Tokyo Night"
-config.window_background_opacity = 0.90
+config.color_scheme = "Nord (Gogh)"
+config.window_background_opacity = 0.87
+config.macos_window_background_blur = 100
 
 config.window_close_confirmation = 'NeverPrompt'
 config.max_fps = 120
